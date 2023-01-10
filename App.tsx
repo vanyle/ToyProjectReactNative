f96a1b9bb5caefde2f30366185b53575f9b4b468
Antoine Delègue <delegue.antoine@gmail.com>
2023-01-10 16:01:15 +0100

    Added jest as a requirement to package.json

--- App.tsx
+++ App.tsx
@@ -21,7 +21,6 @@ import {
 
 import {
   Colors,
-  Header,
 } from "react-native/Libraries/NewAppScreen";
 import CocktailComponent from "./cocktail";
 
@@ -57,23 +56,5 @@ const App: () => Node = () => {
   );
 };
 
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: "600",
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: "400",
-  },
-  highlight: {
-    fontWeight: "700",
-  },
-});
 
 export default App;
